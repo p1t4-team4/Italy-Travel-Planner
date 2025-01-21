668b5383f7912d11d8e2713743ba2f7c8027d550
Amcerna <andrewcerna6@gamil.com>
2025-01-21 16:41:15 -0500

    homepage functionality

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -7,44 +7,90 @@ const italy = document.querySelector("#Italy");
 const cityButtons = document.querySelectorAll('.btn');
 
 const cityNames = [];
-const windows = [];
+let visitedArray  = [];
 
-let cities = [
-    {name: "Rome", visited: false, button: romeButton},
-    {name: "Naples", visited: false, button: naplesButton},
-    {name: "Venice", visited: false, button: veniceButton},
-    {name: "Como", visited: false, button: comoButton},
-    {name: "Milan", visited: false, button: milanButton},
-];
+// const storedTodos = JSON.parse(localStorage.getItem('todos'));
+// localStorage.setItem('todos', JSON.stringify(todos);
 
+
+cities = [
+{name: "Rome", visited: false, button: romeButton},
+{name: "Naples", visited: false, button: naplesButton},
+{name: "Venice", visited: false, button: veniceButton},
+{name: "Como", visited: false, button: comoButton},
+{name: "Milan", visited: false, button: milanButton},
+]
 
 
 for(i = 0; i < cities.length; i++){
     cityNames.push(cities[i].name);
 };
 
+for(i = 0; i < cities.length; i++){
+    visitedArray.push(cities[i].visited);
+}
+
+if(localStorage.getItem('visitedArray') != undefined){
+    visitedArray = JSON.parse(localStorage.getItem('visitedArray'));
+    for(i = 0; i < cities.length; i++){
+        cities[i].visited = visitedArray[i];
+    }
+}
+
+localStorage.setItem('visitedArray', JSON.stringify(visitedArray));
 setCooler();
 
 italy.addEventListener("click", e =>{
-    if (e.target.matches(".btn")){
-        i = cityNames.indexOf(e.target.getAttribute("id"));
-        if(cities[i].visited){
-            cities[i].visited = false;
-        }else{
-            cities[i].visited = true;
-        }
+    if (e.target.matches("#have")){
+        i = cityNames.indexOf(document.querySelector('[status="active"]').getAttribute("id"));
+        cities[i].visited = true;
+        visitedArray[i] = true;
+        localStorage.setItem('visitedArray', JSON.stringify(visitedArray));
         setCooler();
     }
 });
 
 italy.addEventListener("click", e =>{
+    if (e.target.matches("#haveNot")){
+        i = cityNames.indexOf(document.querySelector('[status="active"]').getAttribute("id"));
+        cities[i].visited = false;
+        visitedArray[i] = false;
+        localStorage.setItem('visitedArray', JSON.stringify(visitedArray));
+        setCooler();
+    }
+});
+
+italy.addEventListener("click", e =>{
+    if (e.target.matches("#blog")){
+        i = cityNames.indexOf(document.querySelector('[status="active"]').getAttribute("id"));
+        cityName =  cityNames[i].toLowerCase();
+        window.location.href = `./${cityName}.html`;
+    }
+});
+
+italy.addEventListener("click", e =>{
+    if (e.target.matches("#X")){
+        Win = document.querySelector("#cityWindow");
+        Win.remove();
+        document.querySelector('[status="active"]').setAttribute("status", "inactive");
+        setOpacity()
+    }
+});
+
+italy.addEventListener("click", e =>{
     if (e.target.matches(".btn")){
-        
+
+        i = cityNames.indexOf(e.target.getAttribute("id"));
+
         if(document.querySelector("#cityWindow") != undefined){
             Win = document.querySelector("#cityWindow");
+            button = document.querySelector('[status="active"]');
             Win.remove();
+            button.style.zIndex = 0;
+            button.setAttribute("status", "inactive");
         }
-        i = cityNames.indexOf(e.target.getAttribute("id"));
+        
+        
         cityWindow = document.createElement('div');
         cityWindow.setAttribute("id", "cityWindow");
         X = document.createElement('div');
@@ -61,35 +107,32 @@ italy.addEventListener("click", e =>{
         Blog = document.createElement('div');
         Blog.setAttribute("id", "blog");
         Blog.innerText = "Blog";
+        
         cities[i].button.appendChild(cityWindow);
-        cityWindow.appendChild(X)
+        cities[i].button.setAttribute("status", "active");
+        cities[i].button.style.zIndex = 9;
+        cityWindow.appendChild(X);
         cityWindow.appendChild(yesNo);
         yesNo.appendChild(have);
         yesNo.appendChild(haveNot);
         cityWindow.appendChild(Blog);
+        setOpacity()
         }
-    });
+    }); 
 
-
-
-
-
-
-
-//         if(windows[0] == undefined){
-//             windows.push(document.createElement('div'));
-//             windows[0].innerText = "hello";
-//             cities[i].button.appendChild(windows[0]);
-//             console.log(window[0]);
-//         }else{
-//             windows.pop();
-//             windows[0].innerText = "hello";
-//             cities[i].button.appendChild(windows[0]);
-//             console.log(window[0]);
-//         }
-//     }})
-
-        
+function setOpacity(){
+    inactiveCities = document.querySelectorAll('[status="inactive"]')
+    if(document.querySelector('[status="active"]') == undefined){
+        for(i = 0; i < inactiveCities.length; i++){
+          inactiveCities[i].style.opacity = 1;  
+        }
+    }else{
+        for(i = 0; i < inactiveCities.length; i++){
+            inactiveCities[i].style.opacity = .5;  
+        }
+        document.querySelector('[status="active"]').style.opacity = 1;
+    }
+}
 
 function setCooler(){
     for(i = 0; i < cities.length; i++){
