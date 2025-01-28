3baff803adc3ee73853664fb363b887a575e25c5
Amcerna <andrewcerna6@gamil.com>
2025-01-27 21:56:59 -0500

    1/23 edits
    Co-authored-by: cdgrosso <cdgrosso@users.noreply.github.com>
    Co-authored-by: Gzhub8411 <Gzhub8411@users.noreply.github.com>
    Co-authored-by: DoyleMartin <DoyleMartin@users.noreply.github.com>

--- assets/Js/Blog.js
+++ assets/Js/Blog.js
@@ -1,49 +1,41 @@
 const activitiesInput = document.querySelector('#activitiesText');
 const activitiesForm = document.querySelector('#activitiesForm');
 const activitiesList = document.querySelector('#activitiesList');
-const restaurantsInput = document.querySelector('#restaurantsText');
-const restaurantsForm = document.querySelector('#restaurantsForm');
-const restaurantsList = document.querySelector('#restaurantsList');
-const placesInput = document.querySelector('#placesText');
-const placesForm = document.querySelector('#placesForm');
-const placesList = document.querySelector('#placesList');
 const budgetSection = document.querySelector('#budget');
 const budgetForm = document.querySelector('#budgetForm');
 const itemInput = document.querySelector('#item');
 const priceInput = document.querySelector('#price');
 const budgetList = document.querySelector('#budgetList');
+const editButton = document. querySelector('#editButton');
+const hero = document.querySelector('#hero');
+const facts = document.querySelector('#facts');
 
+Cities=[
+    {Name:"Rome", imagesURLs:[], blrup:"", items:[], plans:[]},
+    {Name:"Naples", imagesURLs:[], blrup:"", items:[], plans:[]},
+    {Name:"Venice", imagesURLs:[], blrup:"", items:[], plans:[]},
+    {Name:"Como", imagesURLs:[], blrup:"", items:[], plans:[]},
+    {Name:"Milan", imagesURLs:[], blrup:"", items:[], plans:[]}
+];
 
 let activities = [];
-let restaurants = [];
-let places = [];
 let budget = [];
 let total = 0;
 
-if (localStorage.getItem('activities') != undefined){
-    activities = JSON.parse(localStorage.getItem('activities'));
-}
-
-if (localStorage.getItem('restaurants') != undefined){
-  restaurants = JSON.parse(localStorage.getItem('restaurants'));
-}
-
-if (localStorage.getItem('places') != undefined){
-  places = JSON.parse(localStorage.getItem('places'));
+if (localStorage.getItem('activities') == undefined){
+   let activitiesList = JSON.parse(localStorage.getItem('activities'));
 }
 
 if (localStorage.getItem('budget') != undefined){
   budget = JSON.parse(localStorage.getItem('budget'));
 }
-
 if (localStorage.getItem('total') != undefined){
   total = localStorage.getItem('total');
 }
 
+
 renderBudget();
 renderActivities();
-renderPlaces();
-renderRestaurants();
 
 activitiesForm.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -84,92 +76,6 @@ activitiesList.addEventListener('click', function (event) {
     activities.splice(index, 1);
     renderActivities();
   }
-});
-
-//Rest
-
-restaurantsForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const restaurantsText = restaurantsInput.value.trim();
-  if (restaurantsText === '') {
-    return;
-  }
-  restaurants.push(restaurantsText);
-  localStorage.setItem('restaurants', JSON.stringify(restaurants));
-  restaurantsInput.value = '';
-
-  renderRestaurants();
-});
-
-function renderRestaurants() {
-  restaurantsList.innerHTML = '';
-
-  for (let i = 0; i < restaurants.length; i++) {
-    const restaurant = restaurants[i];
-
-    const li = document.createElement('li');
-    li.textContent = restaurant;
-    li.setAttribute('data-index', i);
-
-    const button = document.createElement('button');
-    button.textContent = 'Remove';
-
-    li.appendChild(button);
-    restaurantsList.appendChild(li);
-  }
-}
-
-restaurantsList.addEventListener('click', function (event) {
-const element = event.target;
-if (element.matches('button') === true) {
-  const index = element.parentElement.getAttribute('data-index');
-  localStorage.setItem('restaurants', JSON.stringify(restaurants));
-  restaurants.splice(index, 1);
-  renderRestaurants();
-}
-});
-
-//Places
-
-placesForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const placesText = placesInput.value.trim();
-  if (placesText === '') {
-    return;
-  }
-  places.push(placesText);
-  localStorage.setItem('places', JSON.stringify(places));
-  placesInput.value = '';
-
-  renderPlaces();
-});
-
-function renderPlaces() {
-  placesList.innerHTML = '';
-
-  for (let i = 0; i < places.length; i++) {
-    const place = places[i];
-
-    const li = document.createElement('li');
-    li.textContent = place;
-    li.setAttribute('data-index', i);
-
-    const button = document.createElement('button');
-    button.textContent = 'Remove';
-
-    li.appendChild(button);
-    placesList.appendChild(li);
-  }
-}
-
-placesList.addEventListener('click', function (event) {
-const element = event.target;
-if (element.matches('button') === true) {
-  const index = element.parentElement.getAttribute('data-index');
-  localStorage.setItem('places', JSON.stringify(places));
-  places.splice(index, 1);
-  renderPlaces();
-}
 });
 
 budgetForm.addEventListener('submit', function (event) {
